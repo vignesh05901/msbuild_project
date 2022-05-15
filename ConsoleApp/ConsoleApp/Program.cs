d85937ae8404039d4ce9bb20b9650630e1a84b4c
vignesh0590 <vignesh0590@gmail.com>
2022-05-15 21:19:57 +0530

    updates

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
